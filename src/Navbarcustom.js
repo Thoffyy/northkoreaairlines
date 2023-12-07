fce17c991d1d5de3972bdd81cee84b4725116266
Thoffy <bigbigthoffy@proton.me>
2023-12-07 17:18:37 -0300

    adição do html viagens

--- src/Navbarcustom.js
+++ src/Navbarcustom.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import {useState, setCurrentPage} from 'react'
-
 
 const Navbarcustom = () => {
   return (
     <Navbar className='navbarcustom' bg="dark" variant="dark">
       <Navbar.Brand>North Korea Airlines</Navbar.Brand>
-      
       <Nav className="mr-auto" id='textnavbar'>
-        <Nav.Link onClick={() => {setCurrentPage(1);}} >Sobre</Nav.Link>
-        <Nav.Link onClick={() => {setCurrentPage(2);}} >Viagens</Nav.Link>
-        <Nav.Link onClick={() => {setCurrentPage(3);}} >Locação</Nav.Link>
-        <Nav.Link onClick={() => {setCurrentPage(4);}} >Segurança</Nav.Link>
+        <Nav.Link href="teste.html">Sobre</Nav.Link>
+        <Nav.Link href="viagens.html">Viagens</Nav.Link>
+        <Nav.Link href="#blog">Locação</Nav.Link>
+        <Nav.Link href="#notices">Segurança</Nav.Link>
       </Nav>
     
     </Navbar>
