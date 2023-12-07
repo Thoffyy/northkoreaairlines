1d90f656b9435a057ea855dfdcb71d6a4419572f
Thoffy <bigbigthoffy@proton.me>
2023-12-07 14:49:19 -0300

    adicao

--- src/Navbarcustom.js
+++ src/Navbarcustom.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import {useState, setCurrentPage} from 'react'
+
 
 const Navbarcustom = () => {
   return (
     <Navbar className='navbarcustom' bg="dark" variant="dark">
-      <Navbar.Brand>Thoffy's webpage</Navbar.Brand>
+      <Navbar.Brand>North Korea Airlines</Navbar.Brand>
+      
       <Nav className="mr-auto" id='textnavbar'>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#notes">Notes</Nav.Link>
-        <Nav.Link href="#blog">Blog</Nav.Link>
-        <Nav.Link href="#notices">Notices</Nav.Link>
+        <Nav.Link onClick={() => {setCurrentPage(1);}} >Sobre</Nav.Link>
+        <Nav.Link onClick={() => {setCurrentPage(2);}} >Viagens</Nav.Link>
+        <Nav.Link onClick={() => {setCurrentPage(3);}} >Locação</Nav.Link>
+        <Nav.Link onClick={() => {setCurrentPage(4);}} >Segurança</Nav.Link>
       </Nav>
+    
     </Navbar>
   );
 };
